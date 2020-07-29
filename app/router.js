3e88ee324304dfa74ece565d224c2620e7cb099e
Alpeshkumar Prajapati <alpesh1988@gmail.com>
2020-07-29 17:46:20 +0200

    Integrated ember data, modified employees.json structure, Added 'not-found' route

--- app/router.js
+++ app/router.js
@@ -7,5 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  this.route('employee');
   this.route('contact', { path: '/contact-me' });
+  this.route('not-found', { path: '/*path' });
 });
